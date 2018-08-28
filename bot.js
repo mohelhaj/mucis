be1aa1225fe7da32a0d8a1faca92659a3650fb3a
mohelhaj <42706914+mohelhaj@users.noreply.github.com>
2018-08-28 16:55:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,7 +24,7 @@ const prefix = "!";
 /////////////////////////
 ////////////////////////
 
-client.on('message', async msg =>{
+  client.on('message', async msg =>{
 	if (msg.author.bot) return undefined;
     if (!msg.content.startsWith(prefix)) return undefined;
     
